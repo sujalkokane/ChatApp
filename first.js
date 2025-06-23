63b09ed539dd31b8c7f85b57891c9f274fc97074
Sujal Kokane <72248527+sujalkokane@users.noreply.github.com>
2025-06-23 16:39:43 +0530

    Update first.js
    updated

--- first.js
+++ first.js
@@ -10,24 +10,33 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Store usernames for each socket
+const users = {};
+
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  // Notify others
-  socket.broadcast.emit("user connected", "A new user has joined the chat");
-
-  // Listen for chat messages
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  // When user sends their name
+  socket.on("set username", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("user connected", `${username} has joined the chat`);
   });
 
-  // Notify others on disconnect
+  // When user sends a chat message
+  socket.on("chat message", (msg) => {
+    const username = users[socket.id] || "Anonymous";
+    io.emit("chat message", `${username}: ${msg}`);
+  });
+
+  // When user disconnects
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
-    socket.broadcast.emit("user disconnected", "A user has left the chat");
+    const username = users[socket.id] || "A user";
+    socket.broadcast.emit("user disconnected", `${username} has left the chat`);
+    delete users[socket.id];
   });
 });
 
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
+
